b3dd0a289575db40076730aecf59f903d55e4f40
reddysekhar373@gmail.com <reddysekhar373@gmail.com>
2023-01-19 10:03:53 +0530

    Changes commit

---
+++ src/test/java/Login/HomePageTest.java
@@ -0,0 +1,10 @@
+package Login;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is home test");
+
+	}
+
+}
--- src/test/java/Login/LoginPageTest.java
+++ src/test/java/Login/LoginPageTest.java
@@ -3,5 +3,6 @@ package Login;
 public class LoginPageTest {
 	public static void main(String s[]) {
 		System.out.println("Login Page Test Cases");
+		System.out.println("Home Page Test Cases");
 	}
 }
