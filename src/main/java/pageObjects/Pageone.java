e4df2e1c64ba3f352ebea8922ec5b9112c59d676
reddysekhar373@gmail.com <reddysekhar373@gmail.com>
2023-01-20 09:38:03 +0530

    this second change

---
+++ src/main/java/pageObjects/Pageone.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+public class Pageone {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi this is page one method");
+		int i=10;
+		int b=40;
+		System.out.println(i+b);
+
+	}
+
+}
